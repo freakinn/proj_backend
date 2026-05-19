2c13d50de0cf7725c6b10c1a1d154e0ca3d9da25
freakinn <rahuldhandoreeas21b0094@gmail.com>
2026-05-19 22:22:58 +0530

    Allow frontend origin in CORS

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ connectDB();
 const PORT = process.env.PORT || 5173;
 
 app.use(express.json());
-app.use(cors({}));
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://proj-frontend-neon.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+app.options("*", cors());
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
